f117391a531f21f8a6d1584dbf4c8fd72ab11737
Diego Carlos <diego_carlos110@hotmail.com>
2022-10-19 20:19:37 -0300

    adiciono o campo imagefield e template modificado

--- produtos/models.py
+++ produtos/models.py
@@ -5,7 +5,7 @@ class Produto(models.Model):
     titulo = models.CharField(max_length=255)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
-    imagen = models.FileField(upload_to='produtos/', null=True, blank=True)
+    imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
 
     def __str__(self):
         return self.titulo
